225489bb5cb4c214abb00898e96501a7a2cc848c
Carter Beaulieu <CarterBeaulieu@carters-mbp.bc.edu>
2018-09-04 11:57:03 -0400

    2.11

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var myMessage: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,15 +19,46 @@ class ViewController: UIViewController {
     }
 
 
-    @IBAction func myButtonPressed(_ sender: UIButton) {
-        myMessage.text = "You Are Awesome!"
-        myMessage.textColor = UIColor.red
+    @IBAction func showButtonPressed(_ sender: UIButton) {
+        
+        let messages =  ["You Are Awesome!",
+                         "You Are Great!",
+                         "You Are Fantastic!",
+                         "When the Genius Bar needs help, they call you",
+                         "You brighten my day!",
+                         "You are da bomb!",
+                         "Hey, fabulous!",
+                         "You are tremendous!",
+                         "You've got the design skills of Jony Ive!",
+                         "I can't wait to download your app!"]
+        messageLabel.text = messages[index]
+        
+        if index == messages.count - 1
+        {
+            index = 0
+        }
+        else
+        {
+            index += 1
+        }
+        
+  
+//
+//        let message1 = "You Are Awesome!"
+//        let message2 = "You Are Great!"
+//        let message3 = "You Are Amazing!"
+//
+//        if messageLabel.text == message1 {
+//             messageLabel.text = message2
+//        } else if messageLabel.text == message2 {
+//            messageLabel.text = message3
+//        }
+//        else {
+//            messageLabel.text = message1
+//
+//        }
     }
     
-    @IBAction func anotherMessage(_ sender: UIButton) {
-        myMessage.text = "You Are Great!"
-        myMessage.textColor = UIColor.blue
-    }
-    
+   
 }
 
